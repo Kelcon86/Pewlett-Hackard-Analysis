f3512b084e9e2f3e0cf51097ce5485e22be3fd7a
Kelly Conant <KellyC@MacBook-Air.local>
2022-02-13 11:01:24 -0600

    Adding Employee_Database_challenge code.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,38 @@
+-- Deliverable 1: The Number of Retiring Employees by Title
+-- creating Retirement Titles table
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no ASC, to_date DESC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY emp_no ASC, to_date DESC;
+
+-- number of employees by their most recent job title about to retire
+SELECT COUNT(ut.emp_no),
+ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
+
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
+-- creating Mentorship Eligibility Table
 
 -- Deliverable 1: The Number of Retiring Employees by Title
 -- creating Retirement Titles table
@@ -32,24 +67,23 @@ FROM unique_titles as ut
 GROUP BY title 
 ORDER BY COUNT(title) DESC;
 
-SELECT * FROM retiring_titles;
 
-SELECT * FROM unique_titles;
 -- Deliverable 2: The Employees Eligible for the Mentorship Program
 -- creating Mentorship Eligibility Table
-SELECT DISTINCT ON(e.emp_no) e.emp_no, 
-    e.first_name, 
-    e.last_name, 
-    e.birth_date,
-    de.from_date,
-    de.to_date,
-    ti.title
-INTO mentorship_eligibilty
-FROM employees as e
-LEFT OUTER JOIN dept_emp as de
+SELECT DISTINCT ON (emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+JOIN dept_emp AS de
 ON (e.emp_no = de.emp_no)
-LEFT OUTER JOIN titles as ti
-ON (e.emp_no = ti.emp_no)
-WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-AND (de.to_date = '9999-01-01')
-ORDER BY e.emp_no ASC;
+JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE de.to_date = '9999-01-01'
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
